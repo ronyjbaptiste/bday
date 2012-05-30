809885b1b3eac5d6cbee3f2b3d033d0d7d945f52
Rony Jean Baptiste <ronyjbaptiste@gmail.com>
2012-05-30 12:54:04 -0400

    First import

---
+++ login.py
@@ -0,0 +1,24 @@
+import flask, flask.views
+
+users = {'Xman':'2012'}
+
+class Login(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('login.html')
+    
+    def post(self):
+        if 'logout' in flask.request.form:
+            flask.session.pop('username', None)
+            return flask.redirect(flask.url_for('login'))
+        required = ['username', 'passwd']
+        for r in required:
+            if r not in flask.request.form:
+                flask.flash("Error: {0} is required.".format(r))
+                return flask.redirect(flask.url_for('login'))
+        username = flask.request.form['username']
+        passwd = flask.request.form['passwd']
+        if username in users and users[username] == passwd:
+            flask.session['username'] = username
+        else:
+            flask.flash("Username/password incorect")
+        return flask.redirect(flask.url_for('login'))
